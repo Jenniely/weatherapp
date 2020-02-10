d8616ca5dc19a52c036f72a0f15747a6c86001a2
Eugenia Galantseva <lucyatsky@gmail.com>
2020-02-10 14:31:53 -0600

    Updates

---
+++ precache-manifest.3314f60d06dbf573f22d885c6ec68527.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "043e98c71ebd914740f6bfaadb8c0df5",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "5bd69d7f10c1ba052653",
+    "url": "/weatherapp/static/css/main.f7638b57.chunk.css"
+  },
+  {
+    "revision": "f86edad8bc1522786540",
+    "url": "/weatherapp/static/js/2.c011b4cf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weatherapp/static/js/2.c011b4cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5bd69d7f10c1ba052653",
+    "url": "/weatherapp/static/js/main.315e315b.chunk.js"
+  },
+  {
+    "revision": "e4f4b1bc41ffb8c106bc",
+    "url": "/weatherapp/static/js/runtime-main.23a29212.js"
+  }
+]);
